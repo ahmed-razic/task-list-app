2f795f99b3ec9ed2e79d60ac5d799acab2deaa1f
ahmed-razic <ahmed.razic@gmail.com>
2022-04-11 21:34:54 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const taskForm = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task-input');
+const taskFilter = document.querySelector('#task-filter');
+const taskList = document.querySelector('#task-list');
+const taskClear = document.querySelector('#task-clear');
+
+// console.log(taskForm);
+// console.log(taskInput);
+// console.log(taskFilter);
+// console.log(taskList);
+// console.log(taskClear);
